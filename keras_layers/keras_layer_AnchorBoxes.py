60c6fb855ef11633abfd98cc013bbe98808ce88c
sbutylkov <sergey.butylkov@parc.com>
2022-02-11 18:54:13 -0800

    fixes

--- keras_layers/keras_layer_AnchorBoxes.py
+++ keras_layers/keras_layer_AnchorBoxes.py
@@ -19,8 +19,8 @@ limitations under the License.
 from __future__ import division
 import numpy as np
 import keras.backend as K
-from keras.engine.topology import InputSpec
-from keras.engine.topology import Layer
+from tensorflow.python.keras.layers import InputSpec
+from tensorflow.python.keras.layers import Layer
 
 from bounding_box_utils.bounding_box_utils import convert_coordinates
 
--- keras_layers/keras_layer_DecodeDetectionsFast.py
+++ keras_layers/keras_layer_DecodeDetectionsFast.py
@@ -23,8 +23,8 @@ from __future__ import division
 import numpy as np
 import tensorflow as tf
 import keras.backend as K
-from keras.engine.topology import InputSpec
-from keras.engine.topology import Layer
+from tensorflow.python.keras.layers import InputSpec
+from tensorflow.python.keras.layers import Layer
 
 class DecodeDetectionsFast(Layer):
     '''
